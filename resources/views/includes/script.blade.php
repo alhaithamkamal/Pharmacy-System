cbc9107d6bc43365584e9030cbbbb2ee76b38598
logain <logainhassan@github.com>
2020-03-31 15:48:19 +0200

    fix urls of css and js

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,14 +1,14 @@
 <!-- jQuery -->
-<script src="{{ URL::asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ URL::asset('dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
 <!-- DataTables -->
-<script src="{{ URL::asset('plugins/datatables/jquery.dataTables.js') }}"></script>
-<script src="{{ URL::asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('plugins/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{URL::asset('dist/js/demo.js') }}"></script>
+<script src="{{ asset('dist/js/demo.js') }}"></script>
 <!-- page script -->
 <script>
   $(function () {
