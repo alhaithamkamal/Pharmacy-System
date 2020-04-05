f963ef62930b5d60b286d03bd33a64b9e77fc244
nour <nour7anelsto7y@gmail.com>
2020-04-06 00:44:19 +0200

    fix bug

--- app/Client.php
+++ app/Client.php
@@ -11,7 +11,7 @@ class Client extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
-
+    }
     /**
      * The attributes that should be cast to native types.
      *
