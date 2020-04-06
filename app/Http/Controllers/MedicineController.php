8129cbc7e03da5d50b6c5a5bf8c5c3df8c0b735c
sarayasser <sarayasser2008@hotmail.com>
2020-04-06 07:19:16 +0200

    sumodule removed

---
+++ app/Http/Controllers/MedicineController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MedicineController extends Controller
+{
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function show(){
+    	return medicine::all();
+    }
+    public function edit(){
+
+    }
+    public function update(){
+
+    }
+    public function delete(){
+    	
+    }
+}
