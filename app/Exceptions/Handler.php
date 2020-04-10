ccb137b161d0d4c699225db73d9771ae0321f632
logain <logainhassan@github.com>
2020-04-11 00:04:07 +0400

    check authorization when user login

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-
+use Auth;
 class Handler extends ExceptionHandler
 {
     /**
@@ -50,6 +50,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof \Spatie\Permission\Exceptions\UnauthorizedException) {
+            return redirect('login')->with(Auth::logout())->with('error',"it's not authorized to login");
+      }
         return parent::render($request, $exception);
     }
 }
