488b14a68e3665c2b03da9b844bc4ec340b7ae2e
alhaitham <alhaithamkamalali@gmail.com>
2020-04-10 18:06:14 +0200

    order cofirmatio notification

---
+++ app/Notifications/OrderConfirmation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderConfirmation extends Notification
+{
+    use Queueable;
+    private $_order;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($order)
+    {
+        $this->_order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('please confirm or cancel the order.')
+                    ->action('Confirm', url('orders/confirm/'.$this->_order->id))
+                    ->action('Cancel', url('orders/cancel/'.$this->_order->id))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
